3464718c1ba207c9b37c621b01ca803e5dacb85c
George Calin <george.calin@gmail.com>
2020-03-30 17:18:19 +0300

    Exploring the preprocessor

---
+++ Preprocessor.c
@@ -0,0 +1,15 @@
+/* **********************************
+* Using preprocessor directives
+************************************/
+
+
+#include <stdio.h>
+
+#define GREETING "Hello, George!"
+
+int main()
+{
+    puts(GREETING);
+    return (0);
+
+}
