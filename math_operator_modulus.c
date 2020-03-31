bb91911b0daec22f79f35511b46f1b0f8254ff0f
George Calin <george.calin@gmail.com>
2020-03-31 15:36:53 +0300

    Putting math operators at work.

---
+++ math_operator_modulus.c
@@ -0,0 +1,19 @@
+/* *************************************
+* Understand math operator modulus
+************************************** */
+
+
+#include <stdio.h>
+
+int main()
+{
+    int x,a;
+
+    for (x=1;x<=10;x++)
+    {
+        printf("%d % 2 = %d. \n",x,x%2);
+
+    }
+
+     return (0);
+}
---
+++ math_operators_increment_after.c
@@ -0,0 +1,18 @@
+/* *******************************************
+* Understanding math operators a++ and a-- increment and decrement operator
+* ***************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+
+    a=10;
+    b=a;
+
+    printf("The values for a++ and b-- modify the variable after their use. \n Example: a= %d, b=%d \n\n", a++, b--); //expected result a=10, b=10
+    printf("a=%d \n b=%d. \n", a, b); //expected result a=11. b=9
+
+    return (0);
+}
